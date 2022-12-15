72244e24d9af5ed023e1faefcfc43aa86c36854e
JustAlternate <loic.weber@etu.univ-grenoble-alpes.fr>
2022-12-15 09:27:16 +0100

    azer

--- arbresphylo.c
+++ arbresphylo.c
@@ -205,77 +205,13 @@ void afficher_par_niveau (arbre a, FILE* fout) {
 // Acte 4
 //fonction secondaire
 void ajout_direct_carac(arbre* a,char* carac){
-  //printf("ajout_direct_carac\n");
   arbre nouv = nouveau_noeud();
   nouv->droit = *a;
   nouv->valeur = carac;
   *a = nouv;
 }
-
-<<<<<<< HEAD
-
-
-// Acte 5
-
-#define TAILLE_MAX 1000
-
-
-int lire_table(char* nom_fichier, char especes[100][50], char caracteristiques[100][50], int correspondances[100][50], int* nb_especes, int* nb_carac){
-  // On passe en arguments :
-
-  // nom_fichier : le fichier dans lequelle il y a notre arbre
-  // especes : un tableau de strings qui represente la liste des especes (les strings ont une taille max = 1000)
-  // caracteristiques : un tableau de strings qui represente la liste des caracteristiques (les strings ont une taille max = 1000)
-  // correspondances : un tableau de tableau de int qui represente les correspondances d'une caracteristiques pour une espece
-  // exemple correspondances[0][0] == 1 -> espece[0] a la caracteristique caracteristiques[0]
-  // nb_especes : un pointeur vers un int qui contient le nombre d'espece dans le tableau especes
-  // nb_carac : un pointeur vers un int qui contient le nombre de caracteristiques dans le tableau caracteristiques.
-
-  // Renvoie -1 si erreur, 1 si réussi.
-  
-  FILE *f = fopen(nom_fichier, "r") ;
-  if (f == NULL) {
-      printf("%s n'a pas pu être ouvert en lecture \n",nom_fichier) ;
-      return -1 ;
-  }
-  
-  fscanf(f, "%d", nb_especes);
-  printf("Nombre d'especes : %d \n",(*nb_especes));
-  
-
-  printf("Les especes : \n");
-  for (int i=0; i < (*nb_especes); i++){
-    fscanf(f, "%s", especes[i]);
-    printf("%s ",especes[i]);
-  }
-  printf("\n");
-
-  fscanf(f, "%d", nb_carac);
-  printf("Nombre de caracteristiques : %d \n",(*nb_carac));
-  
-
-  printf("Les caracteristiques : \n");
-  for (int i=0; i < (*nb_carac); i++){
-    fscanf(f, "%s", caracteristiques[i]);
-    printf("%s ",caracteristiques[i]);
-  }
-  printf("\n");
-
-
-  for (int i=0; i < (*nb_especes); i++){
-    for (int j=0; j< (*nb_carac); j++){
-      fscanf(f,"%d", &correspondances[j][i]);
-    }
-  } 
-  fclose(f);
-  return 1;
-
-}
-=======
 //fonction principale
 int ajouter_carac_rec(arbre* a, char* carac, cellule_t* seq) {
-  //printf("aa\n");
-  //printf("ajouter_carac_rec(%s, %s, seq)\n", (*a)->valeur, carac);
   if ((*a)->droit == NULL && (*a)->gauche == NULL)
   {
     if(est_dans_seq(seq, (*a)->valeur)){
@@ -348,4 +284,62 @@ int ajouter_carac(arbre* a, char* carac, cellule_t* seq) {
     return 1;
   }
 }
->>>>>>> 1975bc35a904e47104076c4ed8c85715ada8692b
+
+
+
+// Acte 5
+
+#define TAILLE_MAX 1000
+
+
+int lire_table(char* nom_fichier, char especes[100][50], char caracteristiques[100][50], int correspondances[100][50], int* nb_especes, int* nb_carac){
+  // On passe en arguments :
+
+  // nom_fichier : le fichier dans lequelle il y a notre arbre
+  // especes : un tableau de strings qui represente la liste des especes (les strings ont une taille max = 1000)
+  // caracteristiques : un tableau de strings qui represente la liste des caracteristiques (les strings ont une taille max = 1000)
+  // correspondances : un tableau de tableau de int qui represente les correspondances d'une caracteristiques pour une espece
+  // exemple correspondances[0][0] == 1 -> espece[0] a la caracteristique caracteristiques[0]
+  // nb_especes : un pointeur vers un int qui contient le nombre d'espece dans le tableau especes
+  // nb_carac : un pointeur vers un int qui contient le nombre de caracteristiques dans le tableau caracteristiques.
+
+  // Renvoie -1 si erreur, 1 si réussi.
+  
+  FILE *f = fopen(nom_fichier, "r") ;
+  if (f == NULL) {
+      printf("%s n'a pas pu être ouvert en lecture \n",nom_fichier) ;
+      return -1 ;
+  }
+  
+  fscanf(f, "%d", nb_especes);
+  printf("Nombre d'especes : %d \n",(*nb_especes));
+  
+
+  printf("Les especes : \n");
+  for (int i=0; i < (*nb_especes); i++){
+    fscanf(f, "%s", especes[i]);
+    printf("%s ",especes[i]);
+  }
+  printf("\n");
+
+  fscanf(f, "%d", nb_carac);
+  printf("Nombre de caracteristiques : %d \n",(*nb_carac));
+  
+
+  printf("Les caracteristiques : \n");
+  for (int i=0; i < (*nb_carac); i++){
+    fscanf(f, "%s", caracteristiques[i]);
+    printf("%s ",caracteristiques[i]);
+  }
+  printf("\n");
+
+
+  for (int i=0; i < (*nb_especes); i++){
+    for (int j=0; j< (*nb_carac); j++){
+      fscanf(f,"%d", &correspondances[j][i]);
+    }
+  } 
+  fclose(f);
+  return 1;
+
+}
